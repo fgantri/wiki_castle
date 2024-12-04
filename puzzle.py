bd15e9b5685f916878acb71c9c04735bbf9abb14
Fouad Gantri <fouad@gantri.dev>
2024-12-04 19:11:36 +0100

    fix puzzle bug, add better styling

--- puzzle.py
+++ puzzle.py
@@ -10,7 +10,6 @@ warnings.filterwarnings("ignore", category=GuessedAtParserWarning)
 
 import wikipedia
 
-
 wikipedia.set_lang("de")
 
 def get_random_fantasy_article():
@@ -32,25 +31,24 @@ def get_random_fantasy_article():
     while correct_article is None:
         try:
             # Randomly select a title and attempt to fetch the Wikipedia page
-            correct_title = random.choice(fantasy_pages)
-            correct_article = wikipedia.page(correct_title, auto_suggest=False)
+            correct_article = wikipedia.page(random.choice(fantasy_pages), auto_suggest=False)
         except (wikipedia.DisambiguationError, wikipedia.PageError, wikipedia.RedirectError):
             # Skip to the next title if any error occurs
             continue
 
     # Extract some characters
     article_length = len(correct_article.content)
-    character_count = 250
+    character_count = 350
     start_point = random.randint(0, article_length - 1 - character_count)
     snippet = correct_article.content[start_point:start_point + character_count]
 
     # Generate wrong options
-    wrong_options = random.sample([p for p in fantasy_pages if p != correct_title], 3)
+    wrong_options = random.sample([p for p in fantasy_pages if p != correct_article.title], 3)
 
     return {
-        'title': correct_title,
+        'title': correct_article.title,
         'description': '...' + snippet + '...',
-        'options': random.sample([correct_title] + wrong_options, k=4)
+        'options': random.sample([correct_article.title] + wrong_options, k=4)
     }
 
 
@@ -58,7 +56,10 @@ class Puzzle(ABC):
 
     def __init__(self, std_screen):
         curses.init_pair(5, curses.COLOR_CYAN, curses.COLOR_BLACK)
+        curses.init_pair(6, curses.COLOR_WHITE, curses.COLOR_GREEN) # win
+        curses.init_pair(7, curses.COLOR_WHITE, curses.COLOR_RED)  # win
         self._std_screen = std_screen
+        self._user_choice = None
 
     @abstractmethod
     def play(self):
@@ -73,25 +74,47 @@ class SolveTitle(Puzzle):
 
     def play(self):
         self._std_screen.clear()
-
+        screen_height, screen_width = self._std_screen.getmaxyx()
         # Get random article
         article = get_random_fantasy_article()
 
         # Display description
-        self._std_screen.addstr(2, 2,
-            "Erschließe dir den Wikipedia Artikel anhand des folgenden Textes:", curses.A_BOLD | curses.color_pair(5))
-        self._std_screen.addstr(4, 2, article['description'])
+        instruction = "Erschließe dir den Wikipedia Artikel anhand des folgenden Textes:"
+        hr = "##|===========================|##"
+        self._std_screen.addstr(2, screen_width // 2 - len(instruction) // 2,
+            instruction, curses.A_BOLD | curses.color_pair(5))
+        self._std_screen.addstr(4, screen_width // 2 - len(hr) // 2,
+                                hr, curses.A_BOLD | curses.color_pair(5))
+        article_words = article['description'].split(" ")
+        lines = []
+        line = ""
+        word_counter = 0
+        for word in article_words:
+            if word_counter == 15:
+                word_counter = 0
+                lines.append(line)
+                line = ""
+            line += word + " "
+            word_counter += 1
 
-        user_choice = []
+        for i, line in enumerate(lines):
+            self._std_screen.addstr(6 + i, screen_width // 2 - len(line) // 2, line)
+
+        def set_choice(opt):
+            self._user_choice = opt
         # Display options
-        options_menu = Menu(self._std_screen)
-        for i, option in enumerate(article['options'], start=1):
-            options_menu.add_option(option, lambda: user_choice.append(option))
+        options_menu = Menu(self._std_screen, True)
+        for i, option in enumerate(article['options']):
+            options_menu.add_option(option, lambda opt=option: set_choice(opt))
             #self._std_screen.addstr(6 + i * 2, 2, f"{i}. {option}")
-        options_menu.add_option(f"{article['title']}", lambda: user_choice.append(article['title']))
-        options_menu.render(8)
+        options_menu.render()
 
-        if user_choice[0] == article['title']:
-            return True
+        win_message = "*** Herzlichen Glückwunsch :) Du hast dieses Rätsel gelöst. Die Tür ist offen! Drücke eine beliebige Taste... ***"
+        lose_message = f"Leider Falsch! Correct ist {article['title']}. Drücke eine beliebige Taste..."
+        if self._user_choice == article['title']:
+            self._std_screen.addstr(0, screen_width // 2 - len(win_message) // 2, win_message, curses.A_BOLD | curses.color_pair(6))
         else:
-            return False
+            self._std_screen.addstr(0, screen_width // 2 - len(lose_message) // 2, lose_message, curses.A_BOLD | curses.color_pair(7))
+
+        self._std_screen.getch()
+        return self._user_choice == article['title']
